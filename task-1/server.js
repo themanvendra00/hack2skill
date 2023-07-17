c7769d83d230997d8290937c755bf0c7aff32028
themanvendra00 <b.manvendrasingh2000@gmail.com>
2023-07-17 23:07:28 +0530

    created basic api end point and fetch api functionality

--- task-1/server.js
+++ task-1/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 const { connection } = require("./config/db");
 const Data1 = require("./models/data1.model");
 const Data2 = require("./models/data2.model");
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,34 +15,34 @@ app.get("/", (req, res) => {
   res.send("Welcome Hack2Skill team!");
 });
 
-app.get('/fetch-data', (req, res) => {
-    Data1.aggregate([
-      {
-        $lookup: {
-          from: 'data2',
-          localField: 'email',
-          foreignField: 'email',
-          as: 'joinedData',
-        },
+app.get("/fetch-data", (req, res) => {
+  Data1.aggregate([
+    {
+      $lookup: {
+        from: "data2",
+        localField: "email",
+        foreignField: "email",
+        as: "joinedData",
       },
-      {
-        $project: {
-          team_name: { $arrayElemAt: ['$joinedData.team_name', 0] },
-          full_name: 1,
-          email: 1,
-          number: 1,
-          city: 1,
-          url: 1,
-        },
+    },
+    {
+      $project: {
+        team_name: { $arrayElemAt: ["$joinedData.team_name", 0] },
+        full_name: 1,
+        email: 1,
+        number: 1,
+        city: 1,
+        url: 1,
       },
-    ])
-      .then((result) => {
-        res.json(result);
-      })
-      .catch((error) => {
-        res.status(500).json({ error: 'An error occurred' });
-      });
-  });
+    },
+  ])
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "An error occurred" });
+    });
+});
 
 app.listen(port, async () => {
   try {
